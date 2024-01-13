120265b82269729b4d48e1f2d23f5eba22a11156
Arnab-34 <118285743+Arnab-34@users.noreply.github.com>
2024-01-13 13:51:30 +0000

    conversion

---
+++ less1/conversion.js
@@ -0,0 +1,37 @@
+let score = "33";
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber /* its a variable */ = Number(score); // Number(use for typecasting) is an class and the naming convension is Fisrt letter in capital//
+ console.log(valueInNumber);
+ console.log(typeof valueInNumber)
+;
+
+let scores = "33hdw" //let-is a datatype ,scores- is a variabe which stores the data "33hdw"
+
+console.log(typeof(scores));
+let valueInNumber2 = Number(scores);
+console.log(valueInNumber2) // output- Nan(its a special type of datatype)
+console.log(typeof valueInNumber2); //output- number but "33hdw" is not a number(so it is a exception in nodejs/js)
+ 
+// empty string conversion into boolean
+
+let scoress = "";
+console.log(typeof scoress);
+console.log(typeof(scoress));
+
+let valueInNumber3 /* its a variable */ = Boolean(scoress); // Number(use for typecasting) is an class and the naming convension is Fisrt letter in capital//
+ console.log(valueInNumber3);
+ console.log(typeof valueInNumber3)
+;
+
+//string conversion into boolean
+let scoressss = "33";
+console.log(typeof scoressss);
+console.log(typeof(scoressss));
+
+let valueInNumber4 /* its a variable */ = Boolean(scoressss); // Number(use for typecasting) is an class and the naming convension is Fisrt letter in capital//
+ console.log(valueInNumber4);
+ console.log(typeof valueInNumber4)
+;
+
