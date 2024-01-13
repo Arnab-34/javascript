641074232d0175a131c22e914dee7634a74cc7ea
Arnab-34 <118285743+Arnab-34@users.noreply.github.com>
2024-01-13 11:38:57 +0000

    variables

---
+++ less1/varialbles.js
@@ -0,0 +1,10 @@
+const coustomer_ID = "12412"; //constant variable declaration(cannot change the value)//
+let coustomer_email = "aaarnb@gmail.com"; // let is also a variable for 
+var coustomer_password = "1725" /*prefer not to use var */
+let coustomer_city = "kolkata"
+let coustomer_state;
+console.table([coustomer_ID,coustomer_email,coustomer_password, coustomer_city,coustomer_state]);
+// what is scope
+
+//{  }//this curly bracess area is called scope.
+
