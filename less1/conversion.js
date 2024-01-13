d3c599ff9df09956a411c97ddf51799a4738bb95
Arnab-34 <118285743+Arnab-34@users.noreply.github.com>
2024-01-13 20:44:20 +0000

    conversion

--- less1/conversion.js
+++ less1/conversion.js
@@ -35,5 +35,54 @@ let valueInNumber4 /* its a variable */ = Boolean(scoressss); // Number(use for 
  console.log(typeof valueInNumber4)
 ;
 
+//*****************************operations***********
+
+let vulue = 3
+let negetive = -vulue;
+console.log(negetive)// output => -3
+
+console.log(3+45);
+console.log(3*3);
+console.log(3**3);
+console.log(3%3);
+console.log(3/3);// output=> 1  
+
+//ADDITION (string)
+let string1 ="arnab";
+let string2 = " karmakar";
+let string3 = string1 + string2;
+
+console.log(string3);// output=> arnab karmakar.
+
+
+
+console.log("1" + 2);//output=> 12
+console.log("1" +2 +2);//outpur=> 122
+console.log(1 +"2");// output=> 12 
+console.log(2+2+"1");//output=> 41
+//
+
+console.log(true);//output=> true
+console.log(+true);//output=> 1
+console.log(-true);//output=> -1
+console.log(false);//output=> false
+console.log(+false);//output=> 0
+console.log(-false);//output=> -0
+console.log(+"");// output=> 0
+//
+//
+let num1, num2,num3 
+num1=num2=num3=3+3
+console.table([num1,num2,num3]);
+
+// gamecouter operations
+ 
+let gamecounter=23
+const y=gamecounter++;//postfit increment OUTPUT=24
+console.log(`y:${y},gamecounter:${gamecounter}`);
+
+let gamecounter2= 23
+const x= ++gamecounter2// prefix increment OUTPUT=
+console.log(`x:${x},gamecounter2:${gamecounter2}`);
 
 
