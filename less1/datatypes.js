74930da62aec3ecc3c6b147a0161d48fca7c18db
Arnab-34 <118285743+Arnab-34@users.noreply.github.com>
2024-01-13 12:09:42 +0000

    datatypes

---
+++ less1/datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treate all js code as newer version
+// "alert"; it is a data type ,but it not work in nodejs , it is work in browser
+console.log(); //code readability should be high
+"number"
+"bigint"
+"string"
+"boolean"
+"null"// scecial data types
+"undefine"//special data types
+"symbol"//special data types
+"object"//special data types
+
+console.log(typeof undefined);// "typeof" it is use for checking and printing the inpur type or variable type
+console.log(typeof null);
--- less1/varialbles.js
+++ less1/varialbles.js
@@ -1,6 +1,6 @@
 const coustomer_ID = "12412"; //constant variable declaration(cannot change the value)//
 let coustomer_email = "aaarnb@gmail.com"; // let is also a variable for 
-var coustomer_password = "1725" /*prefer not to use var */
+var coustomer_password = "1725" /*prefer not to use var beacuse of issue in block scope and funtions scope */
 let coustomer_city = "kolkata"
 let coustomer_state;
 console.table([coustomer_ID,coustomer_email,coustomer_password, coustomer_city,coustomer_state]);
