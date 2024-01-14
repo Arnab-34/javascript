4e4ccd3f0dec3aa36ec3bf65e53d7cc7f69a9a5c
Arnab-34 <118285743+Arnab-34@users.noreply.github.com>
2024-01-14 08:31:33 +0000

    datatypes

--- less1/datatypes.js
+++ less1/datatypes.js
@@ -12,3 +12,44 @@ console.log(); //code readability should be high
 
 console.log(typeof undefined);// "typeof" it is use for checking and printing the inpur type or variable type
 console.log(typeof null);
+/////////////////////////
+//Symbol data type //it is a special data type use for create uniqueness
+const d=Symbol("2553");
+const andata=Symbol("2553");
+
+console.log(d===andata);
+
+///
+//Bigint usage(after the number"73928899237387923n" we use "n")
+const bigNumber= 73928899237387923n;
+console.log(bigNumber);
+
+
+////////////////////////////////////////////////
+
+//DATA types are two types- PRIMITIVE DATA TYPE AND NON PRIMITIVE OR REFERENCE DATAtype
+
+// primitive are 7 types(call by value) -- "string","Boolean",'Bigint',,"Number","null","undefined".,"symbol".
+//
+// Reference type (call by reference) 3types-
+   //Array, Objects, Functions//
+//EXAMPLE:
+const shib=["neel","shiban","aryan"];
+let Obj =
+{
+name12: "arnab",
+age: 71 };
+console.log(Obj)
+   
+
+//Function returns datatype OBJECT as output
+const  hggh= function () {
+    console.log("hellow World");
+}
+
+   
+
+////QUESTION
+//javascript is a static or dimanic type pogramming language.
+//ANS- DYNAMIC Type//
+
