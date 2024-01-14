9f37cf083ce4bc6b7392d327feb149fe3e1815b1
Arnab-34 <118285743+Arnab-34@users.noreply.github.com>
2024-01-14 19:52:16 +0000

    strings

--- less1/strings.js
+++ less1/strings.js
@@ -12,14 +12,20 @@ console.log(gameNumber.toUpperCase()); // ".toUppercase()"is a type of String me
 console.log(gameNumber.charAt('1'));//".charAt('0')" checking the character in the position. 
 console.log(gameNumber.indexOf('S'));//".indexof('S')" checking the position in the index.
 
-console.log(gameNumber.substring(0,4));
-console.log(gameNumber.slice(-9,4));
-console.log(gameNumber.trim());
-const url = "kneel5400@gmail.com"
+console.log(gameNumber.substring(5));//".substring('')" is a method for print the number 0-4 index position.
+console.log(gameNumber.slice(-9,4));// '".slice('')" is method for print from the negetive index.
+console.log(gameNumber.trim(' '));//".trim('')" is a method for eleminate the blank spacess ine the string.
+const url = "https://kneel5400@gmail.com"// ".url" is a string method to use Url.
 console.log(url);
-console.log(url.includes('kneel'));
+console.log(url.includes('kneel'));//".includes('')"is method use for checking a particular word or sting present or not in the string and gige the OUTPUT as True or False..
 console.log(url.includes('ohds'));
-console.log(url.replace('neel', '20'));
+console.log(url.replace('neel', '20'));//".replace('which string u want to replace','with which word or number')" is a string method 
 
-let Passage="a quick brown fox jumpover the lazy dog"
-console.log(Passage.split(" "));
+let Passage="a quick-brown- fox-jumpover the lazy dog"
+let amm= Passage.split(' ');// split on th e basis on space and 
+console.log(amm[3]);// OUTPUT=>"the" because after spliting all the 
+
+let drek="i love cheap thrils"
+let more= drek.split(' ')
+console.log(drek.indexOf('o'));
+console.log(more[3]);
