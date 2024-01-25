fc6af59227bf4494f1c23c8f8335c6b6907bb9a2
Arnab-34 <118285743+Arnab-34@users.noreply.github.com>
2024-01-25 17:41:56 +0000

    truthy

---
+++ less3/switch.js
@@ -0,0 +1,34 @@
+
+//
+const month =4;
+
+switch (month) {
+    case 1:
+        console.log("January")
+        break;
+    case 2:
+        console.log("February")
+        break;
+    case 3:
+        console.log("March")
+        break;
+    case 4:
+        console.log("April")
+        break;
+    case 4:
+        console.log("May")
+        break;
+    case 6:
+        console.log("june")
+        break;
+    case 7:
+        console.log("july")
+        break;
+    case 8:
+        console.log("august")
+        break;
+
+    default:
+        console.log("invalid month");
+        break;
+}
---
+++ less3/truthy.js
@@ -0,0 +1,30 @@
+const UserEmail ="Anam@gamil.com";
+if(UserEmail){
+    console.log("Got User Email");
+} else{
+    console.log("Donot have User Email");
+}
+
+// Falsy Values=> !,0, -0,"",BinInt 0n,null,undefined,NaN
+//Truthy Values=> 1, "0",'flase'," ",{},function(){}
+
+
+//Check the array is empty or not.
+let Email=[2,4]
+if(Email.length===0){
+console.log("Aarry is Empty");
+}else{
+    console.log("Not an Empty Array");
+}
+
+//
+const emptyObject={
+    arnab:23 ,
+    hemant:34
+}
+if(Object.keys(emptyObject).length===0){
+console.log("the object is empty");
+}else{
+    console.log("the object is not empty");
+    console.log(emptyObject);
+}
