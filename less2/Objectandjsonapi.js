effabc299df2e1e63ff1fea4398301cbb122f219
arnab <arnabkarmakar.cst.2914@gmail.com>
2024-02-18 15:39:44 +0530

    Object and json api

--- less2/Objectandjsonapi.js
+++ less2/Objectandjsonapi.js
@@ -12,3 +12,11 @@ Course.tttt="bmbnb";
 const {OJJHHHHHHH /*actual_key name*/:/*destructuring key name*/oj}=Course;
 console.log(oj);
 
+
+//
+
+let  [a,b,c]=[1,2,[4,5,6]]
+
+console.log(a);
+console.log(b);
+console.log(c);
