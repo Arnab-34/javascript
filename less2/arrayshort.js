c72d635537f3f0909758badf43d0e6c6919bd810
arnab <arnabkarmakar.cst.2914@gmail.com>
2024-02-18 14:58:52 +0530

    arraysort

---
+++ less2/arrayshort.js
@@ -0,0 +1,15 @@
+//Sorting an array in a asending oder.
+// let arr= [15,40,1,13,2] 
+
+// arr.sort((a,b)=>{
+//     return a-b
+// })
+// console.log(arr);
+
+//sorting an array and print in  decending or reverse oder
+let arr= [15,40,1,13,2] 
+arr.sort( (a,b)=> b-a )
+
+console.log( arr);
+
+//
